feb344d3fc86bdf11cb58b02405c48c5527c0309
Aleksei_K <kytykob@gmail.com>
2023-03-14 05:25:05 +0300

    add src/capitalize.js

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,1 +1,10 @@
-js
+export {capitalize};
+
+const capitalize = (text) => {
+    const firstChar = text[0].toUpperCase();
+    const restSubstring = text.slice(1);
+    return `${firstChar}${restSubstring}`;
+  };
+
+
+  //node project-one/src/capitalize.js
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,1 +1,19 @@
-ss
+import { capitalize } from './project-one/src/capitalize.js'
+
+if (capitalize('hello') !== 'Hello') {
+    throw new Error('Функция работает неверно!');
+  }
+  
+  if (capitalize('') !== '') {
+    throw new Error('Функция работает неверно!');
+  }
+  
+  console.log('Все тесты пройдены!');
+
+  
+  //node project-one/tests/capitalize.test.js
+
+
+
+
+  //что то не работает ))
